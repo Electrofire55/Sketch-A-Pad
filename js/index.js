8bfe1e2f48000bfff5540ea9fb5634820733e1a7
Hunter Davis <huntsd123@yahoo.com>
2017-09-15 13:15:34 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 var sizeOfPad = 500;
 var colour = "black" //Default colour of Pixel
-var random = false
-var erase = false
-var recentSize = 16 // Used so addCells function knows previous size of grid.
+var random = false;
+var erase = false;
+var recentSize = 16; // Used so addCells function knows previous size of grid.
 
 $(document).ready(function() {
 
   addCells(recentSize) // Default grid of pixels.
 
-  $("#clear").click(function(){
-    $('#outContainer').effect('shake', {times:2});
-    $("#container").empty();
-    addCells(recentSize);
-  });
+
 
   $("#resize").click(function(){
     $("#container").empty();
-    var size = prompt("Please enter the new size");
+    var size = prompt("Please enter the new size"); //this will also empty the container.
     recentSize = size;
     addCells(size);
+    random = false;
+    erase = false;
+    black = true;
+
   });
 
   $("#randomc").click(function(){
@@ -42,20 +42,20 @@ $(document).ready(function() {
 
 function addCells(size) { // Add's the pixels to the grid.
   var pixelSize = sizeOfPad / size
-  for(x = 0; x < size; x++) { // X axis
-    for(y = 0; y < size; y++) { // Y axis
+  for(x = 0; x < size; x++) {
+    for(y = 0; y < size; y++) {
       $("#container").append("<div id='pixel'> </div>")
     }
   }
 
-  $("[id=pixel]").css({"height":pixelSize,"width":pixelSize}); // Applies CSS to all the pixels
+  $("[id=pixel]").css({"height":pixelSize,"width":pixelSize});
 
-  var mouseDown = false; // Checks when mouse is down on the page.
+  var mouseDown = false;
   $(document).mousedown(function(){ mouseDown = true; }).mouseup(function(){ mouseDown = false; });
 
 
-  $("[id=pixel]").hover(function(){ // $("#pixel") only selects first instance of that id, $("[id=pixel]") selects alls instances.
-    if(mouseDown == true) { // Only if mouse is down on the page.
+  $("[id=pixel]").hover(function(){
+    if(mouseDown == true) {
       if (random == true) { // if random colour is on then change pixel colour randomly.
         colour1 = Math.floor((Math.random() * 255));
         colour2 = Math.floor((Math.random() * 255));
